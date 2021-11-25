e0cd80b0ac7af03b3a298d6c20882f65093108e6
77kkyu <77kkyu@gmail.com>
2021-11-25 23:20:22 +0900

    feat gateway ordered

---
+++ gateway/src/main/java/com/example/gateway/customFilter/OrderedGatewayFilter.java
@@ -0,0 +1,31 @@
+package com.example.gateway.customFilter;
+
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class OrderedGatewayFilter implements GatewayFilter, Ordered {
+
+	private final GatewayFilter delegate;
+	private final int order;
+
+	public OrderedGatewayFilter(GatewayFilter delegate, int order) {
+		this.delegate = delegate;
+		this.order = order;
+	}
+
+	public GatewayFilter getDelegate() {
+		return this.delegate;
+	}
+
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		return this.delegate.filter(exchange, chain);
+	}
+
+	@Override
+	public int getOrder() {
+		return 0;
+	}
+}

f5f27362beba04cf628d0296e1810b5fc09d05b3
77kkyu <77kkyu@gmail.com>
2021-12-03 09:50:38 +0900

    feat searchapi

---
+++ searchApi/src/main/java/com/example/searchapi/SearchApiApplication.java
@@ -0,0 +1,19 @@
+package com.example.searchapi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApiApplication.class, args);
+	}
+
+}
